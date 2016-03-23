0df35165fd3001b8e7338d7e73818f067900bbe0
Kniaziev Oleksandr <knyazev.alexandr@gmail.com>
2016-03-23 15:56:59 +0200

    Added files via upload

--- src/Lists/MainClass.java
+++ src/Lists/MainClass.java
@@ -3,9 +3,26 @@ package Lists;
 public class MainClass {
     public static void main(String[] args) {
         OurList list = new ourArrayList();
-        System.out.println(list.size());
-        list.add(5);
-        System.out.println(list.get(0));
+        //System.out.println(list.size());
+        list.add(5);//0
+        list.add(7);//1
+        list.add(3);//2
+        list.add(6);//3
+        System.out.println("Here should be 7" + list.get(1));
+        for (int i= 0; i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        list.add(3,8);
+
+
+        for (int i= 0; i<list.size();i++){
+            System.out.println("index "+ i +" now value is "+list.get(i));}
+
+        list.remove(4);
+        for (int i= 0; i<list.size();i++){
+            System.out.println("after remove index "+ i +" now value is "+list.get(i));}
+
+
     }
 
 
--- src/Lists/ourArrayList.java
+++ src/Lists/ourArrayList.java
@@ -5,12 +5,24 @@ public class ourArrayList implements OurList{
     public void add(Integer i){
         Integer[] temp = new Integer[arr.length + 1]; //создается новый массив, размером больше на 1 чем оригинальный для дальнейшего добавления нового элемента
         for (int j = 0; j<arr.length;j++){
-            temp[j] = arr[j];//переписывает все элемнты по одному, из массива арр в новый массив
+            temp[j] = arr[j];//переписывает все элементы по одному, из массива арр в новый массив
         }
         temp[temp.length-1]=i; //добавление в конец нового массива нового элемента
-        arr = temp; // перезасеттит ссылку и засеттит новый массив вместо старого
+        arr = temp; // перезасетит ссылку и засеттит новый массив вместо старого
     }
-    public void add(int index, Integer i){} // сделать дома
+    //                    1       4
+    public void add(int index, Integer i){ // сделать дома
+        Integer[] temp = new Integer[arr.length + 1]; //создается новый массив, размером больше на 1 чем оригинальный для дальнейшего добавления нового элемента
+        for (int j = 0; j<arr.length;j++){
+            if(j<index){
+                temp[j]= arr[j];
+            }else{
+            temp[j+1] = arr[j];
+            }
+        }
+        temp[index]=i; //добавление нвого значения по индексу в новый массив
+        arr = temp; // перезасетит ссылку и засеттит новый массив вместо старого
+    }
 
     public void set(int index, Integer i){
         arr[index] = i;
@@ -19,7 +31,16 @@ public class ourArrayList implements OurList{
         return arr[index];
     }
 
-    public void remove(int index){} // сделать дома
+    public void remove(int index){
+        Integer[] temp = new Integer[arr.length - 1];
+        for (int j = 0; j<temp.length;j++){
+            if(j<index){
+                temp[j]= arr[j];
+                      }else {
+                temp[j] = arr[j+1];
+            }
+        }arr = temp;
+    }
 
     public void clear(){
         arr = new Integer[0];
