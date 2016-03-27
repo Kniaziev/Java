e7671e3b2fc8605a88bd7dfa9ba30def33937af8
Kniaziev Oleksandr <knyazev.alexandr@gmail.com>
2016-03-27 17:30:13 +0300

    Added files via upload

--- src/Lists/MainClass.java
+++ src/Lists/MainClass.java
@@ -2,25 +2,59 @@ package Lists;
 
 public class MainClass {
     public static void main(String[] args) {
-        OurList list = new ourArrayList();
+        /*OurList list = new OurArrayList();
         //System.out.println(list.size());
         list.add(5);//0
         list.add(7);//1
         list.add(3);//2
         list.add(6);//3
-        System.out.println("Here should be 7" + list.get(1));
+
         for (int i= 0; i<list.size();i++){
             System.out.println(list.get(i));
         }
-        list.add(3,8);
 
+        list.add(4, 2);
+        System.out.println("New element was added");
+        for (int i= 0; i<list.size();i++){
+            System.out.println(list.get(i));
+        }*/
+
+/*        long before = System.currentTimeMillis();
+        list.add(4,8);
 
         for (int i= 0; i<list.size();i++){
             System.out.println("index "+ i +" now value is "+list.get(i));}
+        long after = System.currentTimeMillis();
+        System.out.println("Time is "+(after - before));*/
 
+
+/*
         list.remove(4);
         for (int i= 0; i<list.size();i++){
-            System.out.println("after remove index "+ i +" now value is "+list.get(i));}
+            System.out.println("after remove index "+ i +" now value is "+list.get(i));}*/
+
+        OurLinkedList linked = new OurLinkedList();
+
+        System.out.println(linked.size());
+
+        linked.add(5);//0
+        linked.add(7);//1
+        linked.add(3);//2
+        linked.add(6);//3
+        for (int i= 0; i<linked.size();i++){
+            System.out.println("linked "+linked.get(i));}
+
+        linked.set(1,4);
+
+        System.out.println("New element was added");
+        for (int i= 0; i<linked.size();i++){
+            System.out.println(linked.get(i));
+        }
+        System.out.println("index is "+ linked.indexOf(4));
+        System.out.println("linked size is "+linked.size());
+        System.out.println(linked.isEmpty());
+
+
 
 
     }
